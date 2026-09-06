12b2b370f49de846bad2c45e570285c89c0d088e
binaryxbeast <prog.rushii@gmail.com>
2026-09-06 15:00:40 +0530

    add Room entity annotations to Wish data class and implement WishDao

---
+++ app/src/main/java/com/example/mywishlist/Data/WishDao.kt
@@ -0,0 +1,29 @@
+package com.example.mywishlist.Data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun addAWish(wishEntity: Wish)
+
+    // Loads all the wishes from the wish table
+    @Query("Select * from `wish-table`")
+    abstract suspend fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateAWish()
+
+    @Delete
+    abstract suspend fun deleteAWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table` where id=:id")
+    abstract fun getAWishById(id: Long): Flow<Wish>
+}
